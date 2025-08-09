54aaf8b1455e83aabf855dae7335e87cb0d61acd
pentapinguin <thepenta007@gmail.com>
2025-08-09 12:32:31 -0600

    Implementando la funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+//Capturar el valor del campo de entrada:
+function agregarAmigo() {
+  nuevoRegistro = document.getElementById("amigo").value;
+  //Validar la entrada:
+  if (document.getElementById("amigo").value != "") {
+    //Actualizar el array de amigos:
+    amigos.push(nuevoRegistro);
+    //Para visualizar que se obtuvo correctamente el dato.
+    console.log(amigos);
+    limpiarCaja();
+    mostrarLista();
+  } else {
+    alert("¡El campo no puede estar vacio!");
+  }
+}
+// Limpiar el campo de entrada:
+function limpiarCaja() {
+  document.getElementById("amigo").value = "";
+}
